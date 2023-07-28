3a7e8c880aa44e93032389931ce387d31fa2b86b
CO14ers54 <CO14ers54@gmail.com>
2023-07-28 13:31:41 -0600

    Add project files.

---
+++ TjcPlaygroundXUnitMoqUat/ShoppingCart.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TjcPlaygroundXUnitMoqUat
+{
+    public record Product(int id, string name, double price);
+
+    public interface IDbService
+    {
+        bool SaveItemToShoppingCart(Product? prod);
+        bool RemoveItemFromShoppingCart(int? prodId);
+    }
+
+
+    public class ShoppingCart
+    {
+        private IDbService m_dbService; 
+
+        public ShoppingCart(IDbService dbService) 
+        {
+            m_dbService = dbService;
+        }
+
+        public bool AddProduct(Product? product)
+        {
+            if (product == null)
+                return false;
+
+            if (product.id == 0)
+                return false;
+
+            m_dbService.SaveItemToShoppingCart(product);
+            return true;
+        }
+
+        public bool DeleteProduct(int? id)
+        {
+            if (id == null) return false;
+
+            if (id == 0) return false;
+
+            m_dbService.RemoveItemFromShoppingCart(id);
+            return true;
+        }
+    }
+}
---
+++ TjcPlaygroundXUnitMoqUat/UserManagement.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TjcPlaygroundXUnitMoqUat
+{
+    public record User(string firstName, string lastName)
+    {
+        public int Id { get; set; }
+        public DateTime CreateDate { get; set; } = DateTime.UtcNow;
+        public string Phone { get; set; }
+        public bool VerifiedEmail { get; set; } = false;
+    }
+
+    public class UserManagement
+    {
+        private readonly List<User> m_users = new();
+
+        private int IdCounter = 1;
+
+        public IEnumerable<User> AllUsers => m_users;
+
+        public void Add(User user)
+        {
+            m_users.Add(user with { Id = IdCounter++ });
+        }
+
+        public void UpdatePhone(User user)
+        {
+            var dbUser = m_users.First(x => x.Id== user.Id);
+        
+            dbUser.Phone = user.Phone;  
+        }
+
+        public void VerifyEmail(int userId)
+        {
+            var dbUser = m_users.First(x => x.Id == userId);
+
+            dbUser.VerifiedEmail = true;
+
+        }
+    }
+}
